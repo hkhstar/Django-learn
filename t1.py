bdb95a161107b5868a72005c7cf7ef46f2333011
hkhstar <hkh.star8@gmail.com>
2025-04-07 14:12:08 +0330

    تمرین 1

---
+++ t1.py
@@ -0,0 +1,47 @@
+# تمرین 1 زوج وفرد
+
+
+
+
+# from random import randint as rnd
+# random_number=rnd(1,9)
+# def odd_or_even(random_number):
+#     if random_number%2 ==0:
+#         print("even")
+#     else:
+#         print("odd")
+# odd_or_even(random_number)
+
+
+
+
+
+# تمرین 2 دناله عددی
+
+
+
+# x=input("Enter a number:")
+# def function(x):
+#     sum=0
+#     for i in range(int(x)):
+#         sum+=(i+1)
+#     return sum    
+# result = function(x)
+# print(result)
+
+
+# تمرین 3 شمارش  عدد و حروف
+
+# def function(x):
+#     in_put=x.replace(" ", "")
+#     count_num=0
+#     count_str=0
+#     for i in in_put:
+#         if i.isnumeric()==True:
+#             count_num+=1
+#         else:
+#             count_str+=1
+#     out={"charcaters":count_str,"numbers":count_num}
+#     return out
+# result = function(input("enter your text: "))
+# print(result)
