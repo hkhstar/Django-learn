d90f9c3603db7175da113b3bb2f4df423cbd1a10
hkhstar <hkh.star8@gmail.com>
2025-04-09 14:11:27 +0330

    t2

---
+++ 2/t2.py
@@ -0,0 +1,34 @@
+#تمرین 1 نام کاربری و پسورد
+
+
+
+# user=input("Please insert your USERNAME: ")
+# if user=="admin":
+#     pass_word=input("Please insert your PASSWOED: ")
+#     if pass_word=="admin":
+#         print("successful login")
+#     else:
+#         print("incorrect password")
+# else:
+#     print("user not found")
+
+
+# تمرین 2 دنباله
+
+
+# for i in range(1,11):
+#     if i!=5:
+#         if i>8:
+#             break
+#         else:
+#             print(i)
+
+
+
+
+
+# تمرین 3  ادغام لیست
+
+# List_1 = [1,2,3,4,5]
+# List_2 = [4,5,6,7,8]
+# print(list(set(List_1).union(set(List_2)))[:3])
