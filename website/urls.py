0995e5ffc2df0105465f1764544565a81636e46c
hkhstar <hkh.star8@gmail.com>
2025-04-30 12:50:55 +0330

    insert template travel
    edit img
    edit js
    edit csss
    urls

--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,8 @@ from django.urls import path
 urlpatterns = [
     
     path('',index_view),
-    path('contact',contact_view),
-    path('about',about_view)
+    path('index.html',index_view),
+    path('contact.html',contact_view),
+    path('about.html',about_view)
 
 ]
