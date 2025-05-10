2579746bf0218fb575a7b88402d40205535dc418
hkhstar <hkh.star8@gmail.com>
2025-05-10 12:54:38 +0330

    test render contaxt

--- website/views.py
+++ website/views.py
@@ -26,4 +26,5 @@ def contact_view(request):
 def about_view(request):
     return render(request,'website/about.html')
 def test_view(request):
-    return render(request,'website/test.html')
+    context={"name":"Hamoun","lastname":"Khatirzad"}
+    return render(request,'website/test.html',context)
