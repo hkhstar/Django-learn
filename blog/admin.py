988ee03be8ad71eaf6c8915d53d338043eac9bb0
hamoun <hamoun.kh@gmail.com>
2025-06-20 12:53:00 +0330

    edit admin panel
    create contact db admin
    edit object view panel
    create Meta class for edit direct db

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import post
 # Register your models here.
+@admin.register(post)
+class postadmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title' ,'counted_views','status','published_date','created_date')
+    list_filter = ('status',)
+    search_fields = ['title','content'] 
+    # ordering=['-created_date']
 
-admin.site.register(post)
+# admin.site.register(post,postadmin)
+
--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Contact 
 # Register your models here.
+class Contactadmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name' ,'email','created_date')
+    list_filter = ('name',)
+    search_fields = ['name','message'] 
+    # ordering=['-created_date']
+admin.site.register(Contact,Contactadmin)
