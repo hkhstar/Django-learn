8105f9f69bbeccf91d8abf536e120050ce3da01a
hamoun <hamoun.kh@gmail.com>
2025-06-27 12:37:29 +0330

    blog set filter
    choose filter show post after now and status  enabled

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import post
-
+from django.utils import timezone
 
 
 def blog_home(request):
-    
-    posts=post.objects.filter(status=1)
+    now=timezone.now()
+    posts = post.objects.filter(published_date__gt=now,status=True).order_by('published_date')
     contexts={'posts':posts}
     return render(request,'blog/blog-home.html',contexts)
+
+
+
 def blog_single(request):
     return render(request,'blog/blog-single.html')
 def test(request,pid):
--- mysite/settings.py
+++ mysite/settings.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+# USE_TZ = True
+# TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
