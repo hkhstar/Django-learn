d8d920bf5cfc327d72b7a20815fbe6a75369ca66
hamoun <hamoun.kh@gmail.com>
2025-07-06 18:07:21 +0330

    add temp;ate tags
    search tag
    category tag
    author tags
    lates tag

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     
     path('',blog_home,name='index'),
     path('<int:pid>',blog_single,name='single'),
+    path('category/<str:cat_name>',blog_home,name='category'),
+    # path('category/<str:cat_name>',blog_category,name='category'),
+    path('author/<str:author_username>',blog_home,name='author'),
+    path('search/',blog_search,name='search'),
     # path('post-<int:pid>',test,name='test'),
   
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,13 @@ from blog.models import post
 from django.utils import timezone
 
 now=timezone.now()
-def blog_home(request):
-    
+def blog_home(request,**kwargs):
     posts = post.objects.filter(published_date__lte=now,status=True).order_by('-published_date')
+    if kwargs.get('cat_name')!=None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username')!= None:
+        posts = posts.filter(author__username=kwargs['author_username'])
+
     contexts={'posts':posts}
     return render(request,'blog/blog-home.html',contexts)
 
@@ -21,6 +25,22 @@ def blog_single(request,pid):
     prev_post = post.objects.filter(id__lt=posts.id,published_date__lte=now, status=True).order_by('-id').first()
     context={'post':posts,'next':next_post,'prev':prev_post}
     return render(request,'blog/blog-single.html',context)
+
+
+def blog_search(request):
+    posts = post.objects.filter(published_date__lte=now,status=True)
+    if request.method=="GET":
+        print(request.GET.get('s'))
+        if s :=request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+    contexts={'posts':posts}
+    return render(request,'blog/blog-home.html',contexts)
+# def blog_category(request,cat_name):
+    
+#     posts = post.objects.filter(published_date__lte=now,status=True).order_by('-published_date')
+#     posts = posts.filter(category__name=cat_name)
+#     contexts={'posts':posts}
+#     return render(request,'blog/blog-home.html',contexts)
 
 
 # def test(request,pid):
