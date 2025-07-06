a9165a41cbae6aafea59a7ce212c9dd1e5f85a4c
hamoun <hamoun.kh@gmail.com>
2025-07-06 20:18:48 +0330

    add tag paginator

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import post
 from django.utils import timezone
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 now=timezone.now()
 def blog_home(request,**kwargs):
@@ -9,6 +10,17 @@ def blog_home(request,**kwargs):
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username')!= None:
         posts = posts.filter(author__username=kwargs['author_username'])
+
+    posts=Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+        
+
 
     contexts={'posts':posts}
     return render(request,'blog/blog-home.html',contexts)
